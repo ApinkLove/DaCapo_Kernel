6006e351e2f5171e1b5463fc37d449f0cab74854
ApinkLove <Jungdeokho@gmail.com>
2015-01-17 16:53:06 +0900

    Update 2015-01-17-07

--- push.sh
+++ push.sh
@@ -4,6 +4,6 @@ rm *.tar zImage
 find . -name "*~" -exec rm {} \;
 cd ..
 git add .
-git commit -m "Update $(date -u +%Y)-$(date -u +%M)-$(date -u +%D)-$(date -u +%H)"
-git push origin master
+git commit -m "Update $(date -u +%Y)-$(date -u +%m)-$(date -u +%d)-$(date -u +%H)"
+git push -f
 
